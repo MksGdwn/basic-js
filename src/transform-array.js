082f8300b579a3b90dbd7566c0bc840d0d9522c8
mksgdwn <mksgdwn@gmail.com>
2023-04-16 17:36:55 +0500

    feat: added transform array solving

--- src/transform-array.js
+++ src/transform-array.js
@@ -13,9 +13,36 @@ const { NotImplementedError } = require('../extensions/index.js');
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
  * 
  */
-function transform(/* arr */) {
-  throw new NotImplementedError('Not implemented');
+function transform(arr) {
+  // throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
+  if(!Array.isArray(arr)) {
+    throw new Error("'arr' parameter must be an instance of the Array!");
+  }
+  const insturction = ['--discard-next', '--discard-prev', '--double-next', '--double-prev'];
+  const result = [];
+  const clone = arr.slice();
+  for(let i = 0; i < clone.length; i++) {
+    if(insturction.includes(clone[i])) {
+      if(insturction.indexOf(clone[i]) == 0) {
+        clone[i + 1] = undefined;
+      } else if(insturction.indexOf(clone[i]) == 1) {
+        result.pop()
+      } else if(insturction.indexOf(clone[i]) == 2) {
+        if(i !== clone.length - 1) {
+          result.push(clone[i + 1]);
+        }
+      } else {
+        if(i !== 0) {
+          result.push(clone[i - 1]);
+        }
+      }
+    } else {
+      result.push(clone[i]);
+    }
+  }
+
+  return result.filter(item => item !== undefined);
 }
 
 module.exports = {
